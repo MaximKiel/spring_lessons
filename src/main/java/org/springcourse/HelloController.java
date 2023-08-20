bd71d2607416b6afe746798c253cff380cc4af1e
MaximKiel <loup.du.nord@yandex.ru>
2023-08-20 22:06:37 +0300

    Create MVC application

---
+++ src/main/java/org/springcourse/HelloController.java
@@ -0,0 +1,13 @@
+package org.springcourse;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class HelloController {
+
+    @GetMapping("/hello-world")
+    public String sayHello() {
+        return "hello_world";
+    }
+}
