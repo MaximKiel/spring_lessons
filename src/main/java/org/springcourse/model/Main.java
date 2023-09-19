f1b41cdf189e5378748ab9c22c13086a8964fe28
MaximKiel <loup.du.nord@yandex.ru>
2023-09-19 21:18:04 +0300

    Add Hibernate ManyToMany functionality with new classes Actor.java and Movie.java

--- src/main/java/org/springcourse/model/Main.java
+++ src/main/java/org/springcourse/model/Main.java
@@ -4,25 +4,45 @@ import org.hibernate.Session;
 import org.hibernate.SessionFactory;
 import org.hibernate.cfg.Configuration;
 
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
 public class Main {
     public static void main(String[] args) {
-        Configuration configuration = new Configuration().addAnnotatedClass(Person.class).addAnnotatedClass(Item.class)
-                        .addAnnotatedClass(Passport.class);
+        Configuration configuration = new Configuration().addAnnotatedClass(Actor.class).addAnnotatedClass(Movie.class);
 
         SessionFactory sessionFactory = configuration.buildSessionFactory();
-        Session session = sessionFactory.getCurrentSession();
 
-        try {
+        try (sessionFactory) {
+            Session session = sessionFactory.getCurrentSession();
             session.beginTransaction();
 
-            Person person = new Person("Alex", 33);
-            Passport passport = new Passport(123456);
-            person.setPassport(passport);
-            session.save(person);
+//            Movie movie = new Movie("Pupl Fiction", 1994);
+//            Actor actor1 = new Actor("Harvey Keitel", 51);
+//            Actor actor2 = new Actor("Samuel L. Jackson", 43);
+//
+//            movie.setActors(new ArrayList<>(List.of(actor1, actor2)));
+//            actor1.setMovies(new ArrayList<>(Collections.singletonList(movie)));
+//            actor2.setMovies(new ArrayList<>(Collections.singletonList(movie)));
+//
+//            session.save(movie);
+//            session.save(actor1);
+//            session.save(actor2);
+
+//            Movie movie = new Movie("Reservoir Dogs", 1992);
+//            Actor actor = session.get(Actor.class, 1);
+//            movie.setActors(new ArrayList<>(Collections.singletonList(actor)));
+//            actor.getMovies().add(movie);
+//            session.save(movie);
+
+            Actor actor = session.get(Actor.class, 2);
+            System.out.println(actor.getMovies());
+            Movie movie = actor.getMovies().get(0);
+            actor.getMovies().remove(0);
+            movie.getActors().remove(actor);
 
             session.getTransaction().commit();
-        } finally {
-            sessionFactory.close();
         }
     }
 }
