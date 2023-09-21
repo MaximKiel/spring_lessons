b92216138bbeae2282cc40de1b30567a6b0c97ff
MaximKiel <loup.du.nord@yandex.ru>
2023-09-21 20:38:59 +0300

    Add item table, add populateDB.sql

---
+++ src/main/resources/populateDB.sql
@@ -0,0 +1,11 @@
+INSERT INTO person(name, age, email) VALUES ('Rick', 34, 'rick@mailcom');
+INSERT INTO person(name, age, email) VALUES ('Mick', 26, 'mick@mailcom');
+INSERT INTO person(name, age, email) VALUES ('Sam', 42, 'sam@mailcom');
+INSERT INTO person(name, age, email) VALUES ('Elon', 50, 'elon@mailcom');
+
+INSERT INTO item(person_id, item_name) VALUES (1, 'IPhone');
+INSERT INTO item(person_id, item_name) VALUES (1, 'Book');
+INSERT INTO item(person_id, item_name) VALUES (5, 'TV');
+INSERT INTO item(person_id, item_name) VALUES (6, 'XBox');
+INSERT INTO item(person_id, item_name) VALUES (7, 'Playstation');
+INSERT INTO item(person_id, item_name) VALUES (8, 'Airpods');
